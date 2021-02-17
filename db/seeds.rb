933e59314995f813a6e7900a9a8f2fb139d4fa7f
Luke Billings <lukecbillings@gmail.com>
2021-02-17 14:54:15 +0000

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+chinatown = { name: "Chinatown", address: "London", phone_number: "01111111", category: "chinese" }
+pastaland = { name: "Pastaland", address: "London", phone_number: "02222222", category: "italian" }
+sushiland = { name: "Sushiland", address: "London", phone_number: "03333333", category: "japanese" }
+franceland = { name: "Franceland", address: "Manchester", phone_number: "04444444", category: "french" }
+belgiumland = { name: "Belgiumland", address: "Liverpool", phone_number: "05555555", category: "belgian" }
+
+[ chinatown, pastaland, sushiland, franceland, belgiumland ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
